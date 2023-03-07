f888adbc61e2f1e4c69e0c6c5a2e94b7ae812810
tammy <tammytamayao@gmail.com>
2023-03-07 14:06:33 +0800

    initialized commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  # devise_for :users, path: '', path_names: {
-  #   sign_in: 'login',``
-  #   sign_out: 'logout',
-  #   registration: 'signup'
-  # },
-  # controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'user/registrations'
-  # }
-
-  devise_for :users
   
   namespace :api do
     namespace :v1 do
