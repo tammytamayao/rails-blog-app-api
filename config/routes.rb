f248fb36f527728f410a9538cbe9a48fe5566ae2
tammy <tammytamayao@gmail.com>
2023-03-07 11:07:13 +0800

    modified routes and recompiled code

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   #   sessions: 'users/sessions',
   #   registrations: 'user/registrations'
   # }
+
+  devise_for :users
   
   namespace :api do
     namespace :v1 do
