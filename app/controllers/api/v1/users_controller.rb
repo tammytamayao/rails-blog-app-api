697ad92419f9fef9d58eae5f3c89353b444513f6
MisterLalusin <misterlalusin@gmail.com>
2023-03-09 14:12:36 +0800

    activation progress

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-
+require 'date'
 
 class Api::V1::UsersController < ApplicationController
     #def index
@@ -14,11 +14,15 @@ class Api::V1::UsersController < ApplicationController
         elsif User.exists?(email: params[:email])
             render :json => {:response => "email already taken"}
         else 
-            @users = User.create(email: params[:email], password_digest: BCrypt::Password.create(params[:password]))
+            
+            random_token = SecureRandom.urlsafe_base64(nil, false)
+            token_exp = DateTime.now.advance(minutes: 2)    
+            
+            body = "Verify your account by clicking on this link " + ENV["URL_REACT"] + "verify?token="+random_token+" before " + token_exp.strftime("%d/%B/%Y %I:%M%p")
+            @users = User.create(email: params[:email], password_digest: BCrypt::Password.create(params[:password]), token_expiration: token_exp ,token: random_token)
             if @users
-                
                 render :json => {:response => "registration successful"}
-                ActionMailer::Base.mail(from: "Rails Blog App", to: params[:email], subject: "Account Verification", body: "Verify your account by clicking on this link " + ENV["URL_REACT"] + "verify?token=").deliver
+                ActionMailer::Base.mail(from: "Rails Blog App", to: params[:email], subject: "Account Verification", body: body).deliver
 
             else
                 render json: @users.errors
@@ -36,16 +40,18 @@ class Api::V1::UsersController < ApplicationController
         #logger.info("Logs" + ca.password_digest)
         #if User.where(:id => (ca.id & cb.id))
 
-        
-
-        if BCrypt::Password.new(ca.password_digest) === BCrypt::Engine.hash_secret(params[:password], ca.password_digest)  
-            #render :json => {:response => "authenticated" }
-            if ca.confirmed_at === nil
-                render :json => {:response => "email confirmation required" }
-            else
-                render :json => {:response => "authenticated" }
+        begin            
+            if BCrypt::Password.new(ca.password_digest) === BCrypt::Engine.hash_secret(params[:password], ca.password_digest)  
+                #render :json => {:response => "authenticated" }
+                if ca.confirmed_at === nil
+                    render :json => {:response => "email confirmation required" }
+                else
+                    render :json => {:response => "authenticated" }
+                end
+            else 
+                render :json => {:response => "log in failed"}
             end
-        else 
+        rescue Exception => e
             render :json => {:response => "log in failed"}
         end
         
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_040555) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_053428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_040555) do
     t.datetime "updated_at", null: false
     t.datetime "confirmed_at", precision: nil
     t.string "password_digest", null: false
+    t.datetime "token_expiration"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
